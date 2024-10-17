b2d46d2c75d6d0f0844e635d681fcbae87b23568
loricode <loritec26@gmail.com>
2024-10-17 14:43:51 -0500

    agregando servidor

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+//For env File 
+dotenv.config();
+
+const app = express();
+
+app.set('port', process.env.PORT || 4000 );
+
+app.get('/', (req, res) => {
+   res.send('Inicio del portafolio');
+});
+
+app.listen(app.get('port'), () => {
+
+   console.log("listening express")
+
+})
