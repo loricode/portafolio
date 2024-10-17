a7ed0f24fb0344032f1241ed036896481923122f
loricode <loritec26@gmail.com>
2024-10-17 15:02:50 -0500

    archivo html

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 //For env File 
 dotenv.config();
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.set('port', process.env.PORT || 4000 );
 
 app.set("view options", {layout: false});
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(join(__dirname + '/public')));
 
 app.get('/', (req, res) => {
+   console.log(__dirname, dirname)
    res.render('index.html');
 });
 
